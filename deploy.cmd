1e07f77fa13f5fc13057be968f95ca1efab6e8aa
InKolev <in.kolev.b@gmail.com>
2015-11-14 01:52:46 +0200

    Deployment part II.

--- deploy.cmd
+++ deploy.cmd
@@ -1,11 +1,11 @@
     @echo off
-    IF "%SITE_FLAVOR%" == "nodejs" (
-      deploy.node.cmd
+    IF "%SITE_FLAVOR%" == "Server" (
+      deploy.Server.cmd
     ) ELSE (
-      IF "%SITE_FLAVOR%" == "mvc4" (
-        deploy.mvc4.cmd
+      IF "%SITE_FLAVOR%" == "UI" (
+        deploy.UI.cmd
       ) ELSE (
-        echo You have to set SITE_FLAVOR setting to either "nodejs" or "mvc4"
+        echo You have to set SITE_FLAVOR setting to either "Server" or "UI"
         exit /b 1
       )
     )
